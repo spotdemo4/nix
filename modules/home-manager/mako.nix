5131f0504c8c3ef16dc153fad24e9dd9ba204b31
trev <me@trev.xyz>
2024-06-10 02:43:02 -0400

    167 current  2024-06-10 02:43:00  24.11.20240531.57610d2  6.6.32                          *

--- modules/home-manager/mako.nix
+++ modules/home-manager/mako.nix
@@ -8,6 +8,7 @@
   config = lib.mkIf config.mako-conf.enable {
     services.mako = {
       enable = true;
+      defaultTimeout = 5000;
       catppuccin = {
         enable = true;
         flavor = "mocha";
