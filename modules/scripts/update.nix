6cacf7a246af6302af8471a1d2bfded473c2dca5
trev <me@trev.xyz>
2024-08-23 18:21:06 -0400

    235 current  2024-08-23 18:21:04  24.11.20240821.c374d94  6.6.47                          *

--- modules/scripts/update.nix
+++ modules/scripts/update.nix
@@ -13,13 +13,18 @@
         runtimeInputs = with pkgs; [ git ];
 
         text = ''
+          if [ -z "$1" ]; then
+            echo "Usage: update <host name>"
+            exit 1
+          fi
+
           pushd /etc/nixos
           echo "NixOS Rebuilding..."
           sudo nix flake update
           
           sudo git add .
 
-          sudo nixos-rebuild switch --flake /etc/nixos#default
+          sudo nixos-rebuild switch --flake "/etc/nixos#$1"
 
           echo "Waiting for network..."
           until ping -c1 www.google.com >/dev/null 2>&1; do :; done
