6dd50ed9b2f0d43083bb7c24c1698e538d3e74b8
trev <me@trev.xyz>
2024-08-24 06:17:00 -0400

    18 current  2024-08-24 06:16:58  24.11.20240821.c374d94  6.6.47                          *

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -9,106 +9,23 @@
     inputs.home-manager.nixosModules.home-manager
     inputs.catppuccin.nixosModules.catppuccin
     ./hardware-configuration.nix
-
-    # Programs
-    ../../modules/nixos/hyprland.nix
-    ../../modules/nixos/sddm.nix
-    ../../modules/nixos/pipewire.nix
-    ../../modules/nixos/gnome-auth-agent.nix
-    ../../modules/nixos/git.nix
-    ../../modules/nixos/syncthing.nix
-    ../../modules/nixos/postgres.nix
-
-    # Scripts
-    ../../modules/scripts/update.nix
+  ] ++ map (x: ./../../modules/nixos/${x}.nix) [
+    # Programs to import
+    "git"
+    "gnome-auth-agent"
+    "hyprland"
+    "pipewire"
+    "postgres"
+    "sddm"
+    "steam"
+    "syncthing"
+    "zsh"
+  ] ++ map (x: ./../../modules/scripts/${x}.nix) [
+    # Scripts to import
+    "update"
   ];
 
-  # Boot loader
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.timeout = 10;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  # Console
-  console.catppuccin = {
-    enable = true;
-    flavor = "mocha";
-  };
-
-  # Flakes
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  # Auto Upgrade
-  system.autoUpgrade = {
-    enable = true;
-    flake = inputs.self.outPath;
-    flags = [
-      "--update-input"
-      "nixpkgs"
-      "-L" # print build logs
-    ];
-    dates = "02:00";
-    randomizedDelaySec = "45min";
-  };
-
-  # Networking
-  networking.hostName = "nixos"; # Define your hostname.
-  networking.networkmanager.enable = true;
-  networking.firewall.enable = false;
-  services.tailscale.enable = true;
-
-  # Bluetooth
-  hardware.bluetooth.enable = true;
-  hardware.bluetooth.powerOnBoot = true;
-  services.blueman.enable = true;
-
-  # Security
-  security.polkit.enable = true;
-
-  # Time zone
-  time.timeZone = "America/Detroit";
-
-  # Internationalisation properties
-  i18n.defaultLocale = "en_US.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
-
-  # User accounts
-  users.users.trev = {
-    isNormalUser = true;
-    description = "trev";
-    extraGroups = [ "networkmanager" "wheel" "docker" ];
-    packages = with pkgs; [];
-    shell = pkgs.zsh;
-  };
-
-  # Home manager
-  home-manager = {
-    useGlobalPkgs = true;
-    extraSpecialArgs = { inherit inputs; };
-    users = {
-      trev.imports = [ ./trev.nix ];
-    };
-  };
-
-  # Docker
-  virtualisation.docker.enable = true;
-
-  # Allow unfree packages and add overlays
-  nixpkgs = {
-    overlays = [ inputs.catppuccin-vsc.overlays.default ];
-    config.allowUnfree = true;
-  };
-
-  # List packages installed in system profile. To search, run nix search wget
+  # Packages to install
   environment.systemPackages = with pkgs; [
     # GUI
     nemo
@@ -150,25 +67,98 @@
     hyprpaper
   ];
 
-  # Fonts
+  # Fonts to install
   fonts.packages = with pkgs; [
     meslo-lgs-nf
     fira-code
   ];
 
-  # Programs
-  programs.zsh.enable = true;
-  programs.steam.enable = true;
-  hyprland-nix.enable = true;
-  sddm-nix.enable = true;
-  pipewire-nix.enable = true;
-  gnome-auth-agent-nix.enable = true;
-  git-nix.enable = true;
-  syncthing-nix.enable = true;
-  postgres-nix.enable = true;
+  # -- SYSTEM CONFIGURATION --
 
-  # Scripts
-  update-script.enable = true;
+  # Boot loader
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.timeout = 10;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Console
+  console.catppuccin = {
+    enable = true;
+    flavor = "mocha";
+  };
+
+  # Flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Auto Upgrade
+  system.autoUpgrade = {
+    enable = true;
+    flake = inputs.self.outPath;
+    flags = [
+      "--update-input"
+      "nixpkgs"
+      "-L" # print build logs
+    ];
+    dates = "02:00";
+    randomizedDelaySec = "45min";
+  };
+
+  # Networking
+  networking.hostName = "nixos-desktop"; # Define your hostname.
+  networking.networkmanager.enable = true;
+  networking.firewall.enable = false;
+  services.tailscale.enable = true;
+
+  # Bluetooth
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+  services.blueman.enable = true;
+
+  # Security
+  security.polkit.enable = true;
+
+  # Time zone
+  time.timeZone = "America/Detroit";
+
+  # Internationalisation properties
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Home manager
+  home-manager = {
+    useGlobalPkgs = true;
+    extraSpecialArgs = { inherit inputs; };
+    users = {
+      trev.imports = [ ./trev.nix ];
+    };
+  };
+
+  # User accounts
+  users.users.trev = {
+    isNormalUser = true;
+    description = "trev";
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    packages = with pkgs; [];
+    shell = pkgs.zsh;
+  };
+
+  # Docker
+  virtualisation.docker.enable = true;
+
+  # Allow unfree packages and add overlays
+  nixpkgs = {
+    config.allowUnfree = true;
+    overlays = [ inputs.catppuccin-vsc.overlays.default ];
+  };
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -177,5 +167,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
-
 }
