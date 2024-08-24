e46ff8af1b13d70ae00ba853c309c22c5419e00f
trev <me@trev.xyz>
2024-08-24 08:20:55 -0400

    24 current  2024-08-24 08:20:51  24.11.20240821.c374d94  6.6.47                          *

--- modules/scripts/rebuild.nix
+++ modules/scripts/rebuild.nix
@@ -15,18 +15,18 @@
 
         pushd /etc/nixos
 
-        printf "\033[4;36m Getting most recent good flake.lock...\n"
+        printf "\033[0;36mGetting most recent flake.lock...\n\033[0m"
         sudo git fetch
         sudo git checkout origin/main -- flake.lock
 
-        printf "\033[4;36m NixOS Rebuilding...\n"
+        printf "\033[0;36mNixOS Rebuilding...\n\033[0m"
         sudo git add .
         sudo nixos-rebuild switch --flake "/etc/nixos#$1"
 
-        printf "\033[4;36m Waiting for network...\n"
+        printf "\033[0;36mWaiting for network...\n\033[0m"
         until ping -c1 www.google.com >/dev/null 2>&1; do :; done
 
-        printf "\033[4;36m Committing and pushing...\n"
+        printf "\033[0;36mCommitting and pushing...\n\033[0m"
         gen=$(nixos-rebuild list-generations | grep current)
         sudo git commit -m "$gen"
         sudo git push -u origin main
