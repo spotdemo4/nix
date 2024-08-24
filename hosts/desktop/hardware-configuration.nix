253975ae4e0c18664c499f58ef69923b2ad34c78
trev <me@trev.xyz>
2024-08-24 06:33:53 -0400

    19 current  2024-08-24 06:33:51  24.11.20240821.c374d94  6.6.47                          *

--- hosts/desktop/hardware-configuration.nix
+++ hosts/desktop/hardware-configuration.nix
@@ -8,6 +8,12 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
+  
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
