e0e6172c5de9dff30ab3a46c4fa47e14ecd0a573
trev <me@trev.xyz>
2024-08-26 16:36:18 -0400

    37 current  2024-08-26 16:36:15  24.11.20240824.d0e1602  6.6.47                          *

--- modules/home-manager/hyprpaper.nix
+++ modules/home-manager/hyprpaper.nix
@@ -13,6 +13,8 @@
       wallpaper = eDP-1,~/Photos/wallpaper.jpg
       wallpaper = desc:GIGA-BYTE TECHNOLOGY CO. LTD. G34WQC A 23072B001686,~/Photos/fishy.png
 
+      unload unused
+
       ipc = off
     '';
   };
