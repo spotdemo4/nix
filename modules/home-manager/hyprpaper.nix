5a01c246b8dbb892a11aec5b0b73ddaa3f0fb2f3
trev <me@trev.xyz>
2024-08-26 16:34:26 -0400

    36 current  2024-08-26 16:34:22  24.11.20240824.d0e1602  6.6.47                          *

--- modules/home-manager/hyprpaper.nix
+++ modules/home-manager/hyprpaper.nix
@@ -8,7 +8,10 @@
   xdg.configFile = {
     "hypr/hyprpaper.conf".text = ''
       preload = ~/Photos/wallpaper.jpg
+      preload = ~/Photos/fishy.png
+
       wallpaper = eDP-1,~/Photos/wallpaper.jpg
+      wallpaper = desc:GIGA-BYTE TECHNOLOGY CO. LTD. G34WQC A 23072B001686,~/Photos/fishy.png
 
       ipc = off
     '';
