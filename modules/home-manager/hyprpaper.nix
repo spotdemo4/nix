59d25880135981d8edfa6e4f0aea9c746c911d59
trev <me@trev.xyz>
2024-08-26 17:11:26 -0400

    40 current  2024-08-26 17:11:17  24.11.20240824.d0e1602  6.6.47                          *

--- modules/home-manager/hyprpaper.nix
+++ modules/home-manager/hyprpaper.nix
@@ -9,11 +9,11 @@
     "hypr/hyprpaper.conf".text = ''
       preload = ~/Photos/wallpaper.jpg
       preload = ~/Photos/fishy.png
-      preload = ~/Photos/pond.jpg
+      preload = ~/Photos/fishy2.jpg
 
       wallpaper = eDP-1,~/Photos/wallpaper.jpg
       wallpaper = desc:GIGA-BYTE TECHNOLOGY CO. LTD. G34WQC A 23072B001686,~/Photos/fishy.png
-      wallpaper = desc:Dell Inc. S2719DGF 1HSYBY2,~/Photos/pond.jpg
+      wallpaper = desc:Dell Inc. S2719DGF 1HSYBY2,~/Photos/fishy2.jpg
 
       ipc = off
     '';
