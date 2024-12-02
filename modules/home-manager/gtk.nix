fa969025c7f1f9df8d30d51193ae39a1f5eba5b8
trev <me@trev.xyz>
2024-12-02 02:14:43 -0500

    97 current  2024-12-02 02:14:38  25.05.20241201.ac35b10  6.6.63                          *

--- modules/home-manager/gtk.nix
+++ modules/home-manager/gtk.nix
@@ -3,20 +3,25 @@
 {
   gtk = {
     enable = true;
-    theme = {
-      package = pkgs.colloid-gtk-theme.override {
-        tweaks = ["catppuccin"];
-        colorVariants = ["dark"];
-        themeVariants = ["teal"];
+    # theme = {
+    #   package = pkgs.colloid-gtk-theme.override {
+    #     tweaks = ["catppuccin"];
+    #     colorVariants = ["dark"];
+    #     themeVariants = ["teal"];
+    #   };
+    #   name = "Colloid-Teal-Dark-Catppuccin";
+    # };
+
+    catppuccin = {
+      enable = true;
+      accent = "sky";
+      flavor = "mocha";
+
+      icon = {
+        enable = true;
+        accent = "sky";
+        flavor = "mocha";
       };
-      name = "Colloid-Teal-Dark-Catppuccin";
-    };
-    iconTheme = {
-      package = pkgs.colloid-icon-theme.override {
-        schemeVariants = ["catppuccin"];
-        colorVariants = ["teal"];
-      };
-      name = "Colloid-Teal-Catppuccin-Dark";
     };
   };
 }
