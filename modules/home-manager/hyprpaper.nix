b7817609c0041527b23cd6dacdceefc9ea7d2839
trev <me@trev.xyz>
2024-12-11 11:39:01 -0500

    252 current  2024-12-11 11:38:58  25.05.20241209.a73246e  6.6.63                          *

--- modules/home-manager/hyprpaper.nix
+++ modules/home-manager/hyprpaper.nix
@@ -1,21 +1,44 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [
-    hyprpaper
-  ];
+  # home.packages = with pkgs; [
+  #   hyprpaper
+  # ];
 
-  xdg.configFile = {
-    "hypr/hyprpaper.conf".text = ''
-      preload = ~/Photos/wallpaper.jpg
-      preload = /etc/nixos/static/fishy.png
-      preload = /etc/nixos/static/fishy_vertical.png
+  # xdg.configFile = {
+  #   "hypr/hyprpaper.conf".text = ''
+  #     preload = ~/Photos/wallpaper.jpg
+  #     preload = /etc/nixos/static/fishy.png
+  #     preload = /etc/nixos/static/fishy_vertical.png
 
-      wallpaper = eDP-1,~/Photos/wallpaper.jpg
-      wallpaper = desc:GIGA-BYTE TECHNOLOGY CO. LTD. G34WQC A 23072B001686,/etc/nixos/static/fishy.png
-      wallpaper = desc:Dell Inc. S2719DGF 1HSYBY2,/etc/nixos/static/fishy_vertical.png
+  #     wallpaper = eDP-1,~/Photos/wallpaper.jpg
+  #     wallpaper = desc:GIGA-BYTE TECHNOLOGY CO. LTD. G34WQC A 23072B001686,/etc/nixos/static/fishy.png
+  #     wallpaper = desc:Dell Inc. S2719DGF 1HSYBY2,/etc/nixos/static/fishy_vertical.png
 
-      ipc = off
-    '';
+  #     ipc = off
+  #   '';
+  # };
+
+  services.hyprpaper = {
+    enable = true;
+    settings = {
+      preload = [
+        "/etc/nixos/static/fishy.png"
+        "/etc/nixos/static/fishy1080.png"
+        "/etc/nixos/static/fishy_vertical.png"
+      ];
+
+      wallpaper = [
+        #Laptop
+        "eDP-1,/etc/nixos/static/fishy1080.png"
+
+        #Work
+        "desc:Samsung Electric Company S34J55x H4LT901888,/etc/nixos/static/fishy.png"
+
+        # Home
+        "desc:GIGA-BYTE TECHNOLOGY CO. LTD. G34WQC A 23072B001686,/etc/nixos/static/fishy.png"
+        "desc:Dell Inc. S2719DGF 1HSYBY2,/etc/nixos/static/fishy_vertical.png"
+      ];
+    };
   };
 }
