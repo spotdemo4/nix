f67a4199494c1aacedfe652dc77d3deeef6a83ac
trev <me@trev.xyz>
2024-12-20 06:59:19 -0500

    124 current  2024-12-20 06:59:17  25.05.20241217.d3c42f1  6.6.66                          *

--- modules/nixos/hyprland.nix
+++ modules/nixos/hyprland.nix
@@ -1,8 +1,15 @@
 { pkgs, inputs, ... }:
  
 {
+  # Use Cachix https://wiki.hyprland.org/Nix/Cachix/
+  nix.settings = {
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
+
   programs.hyprland = {
     enable = true;
     package = inputs.hyprland.packages."${pkgs.system}".hyprland;
+    portalPackage = inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland;
   };
 }
