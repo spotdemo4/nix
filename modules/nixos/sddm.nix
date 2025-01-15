5cea0dd9eb4c2da974c782470d41257e155a8a0c
trev <me@trev.xyz>
2025-01-14 20:46:59 -0500

    261 current  2025-01-14 20:46:56  25.05.20250113.9abb87b  6.6.71                          *

--- modules/nixos/sddm.nix
+++ modules/nixos/sddm.nix
@@ -4,17 +4,17 @@
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
-    theme = "where_is_my_sddm_theme_qt5";
+    package = pkgs.kdePackages.sddm;
   };
 
-  environment.systemPackages = [
-    (pkgs.where-is-my-sddm-theme.override {
-      variants = [ "qt5" ];
-    })
-  ];
+  # environment.systemPackages = [
+  #   (pkgs.where-is-my-sddm-theme.override {
+  #     variants = [ "qt5" ];
+  #   })
+  # ];
 
-  #catppuccin.sddm = {
-  #  enable = true;
-  #  flavor = "mocha";
-  #};
+  catppuccin.sddm = {
+   enable = true;
+   flavor = "mocha";
+  };
 }
