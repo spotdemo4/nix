c321e6a1a833f067f0110378cdbfd74f5d37b342
trev <me@trev.xyz>
2025-02-21 14:54:03 -0500

    185 current  2025-02-21 14:54:00  25.05.20250218.73cf49b  6.12.14                          *

--- modules/home-manager/hyprland.nix
+++ modules/home-manager/hyprland.nix
@@ -102,8 +102,8 @@
 
       # Group config 
       group = {
-        "col.border_active" = "rgba(ff9900ee) rgba(ff1a00ee) 45deg";
-        "col.inactive_border" = "rgba(595959aa)";
+        "col.active" = "rgba(ff9900ee) rgba(ff1a00ee) 45deg";
+        "col.inactive" = "rgba(595959aa)";
 
         groupbar = {
           "stacked" = true;
