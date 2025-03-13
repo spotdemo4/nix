992baf497dfcb1e3dbda282b0994b34fb39a68fd
trev <me@trev.xyz>
2025-03-13 06:34:09 -0400

    241 current  2025-03-13 06:34:07  25.05.20250309.e3e32b6  6.12.18                          *

--- modules/home-manager/vscode.nix
+++ modules/home-manager/vscode.nix
@@ -9,17 +9,11 @@
         jnoortheen.nix-ide
         usernamehw.errorlens
         golang.go
-        gleam.gleam
-        ms-vscode.cpptools
-        ziglang.vscode-zig
       ]) ++ (with inputs.nix-vscode-extensions.extensions.x86_64-linux; [
         vscode-marketplace.svelte.svelte-vscode
         vscode-marketplace.continue.continue
-        vscode-marketplace.a-h.templ
-        vscode-marketplace.trapfether.tailwind-raw-reorder
-        vscode-marketplace.pcbowers.alpine-intellisense
-        vscode-marketplace.peterj.proto
         vscode-marketplace.bradlc.vscode-tailwindcss
+        vscode-marketplace.zxh404.vscode-proto3
       ]) ++ [
         (pkgs.catppuccin-vsc.override {
           accent = "sky";
@@ -35,8 +29,6 @@
         "editor.fontFamily" = "Fira Code";
         "editor.fontLigatures" = true;
         "svelte.enable-ts-plugin" = true;
-        "alpine-intellisense.settings.languageScopes" = "html,templ";
-        "tailwind-raw-reorder.IgnoreConfigNotFound" = true;
       };
     };
     mutableExtensionsDir = false;
