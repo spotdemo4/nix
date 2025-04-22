eb9d60b321a613ea5b87a7789ba749ca717a40f9
trev <me@trev.xyz>
2025-04-22 13:32:03 -0400

    282 current  2025-04-22 13:29:05  25.05.20250417.b024ced  6.12.23                          *

--- modules/home-manager/authorized-keys.nix
+++ modules/home-manager/authorized-keys.nix
@@ -1,6 +1,6 @@
 {...}: {
   home.file = {
-    ".ssh/authorized_keys" = ''
+    ".ssh/authorized_keys".text = ''
       ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILhbWUnHfLabigfXHSpkVv1YdrGSAoB0KXp23BsW+cZs trev@desktop
       ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINYjEopsO508BUVU2wu/RUP97psEdxzUhqH+kvvj2M8x trev@laptop
     '';
