2bffd9fc377faa8669c4564f91f4daaf1c4e103e
trev <me@trev.xyz>
2025-05-19 15:24:13 -0400

    fix: rekey

--- secrets/keys.nix
+++ secrets/keys.nix
@@ -7,7 +7,7 @@ let
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAxD3eccbwfEkahm6zLR+JIVnshwSBFO3dX3roFHndgp root@build"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIInThVD92YRSlKIxCdhqLwsGkmvRUvRQFHwOuCQEOQlh root@media"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGQeBo8YspGiTDH3xhIg0vTWzwIOJtk3VeE6PQ97lyu4 root@gateway"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEzq4G23A+En47JYla4RTSB3fWwKXqJ7tSONmIHTOloh root@monitor"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOFm+wetRXHfaW8yace4qW/zYG2+cqDWgTPgCOm8WHeX root@monitor"
   ];
 in {
   local = local_keys;
