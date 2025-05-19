eebcc99a7a8b234876d2db7a01fedff37f6ba89e
trev <me@trev.xyz>
2025-05-18 21:48:39 -0400

    fix: use correct public key

--- secrets/keys.nix
+++ secrets/keys.nix
@@ -4,7 +4,7 @@ let
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINYjEopsO508BUVU2wu/RUP97psEdxzUhqH+kvvj2M8x trev@laptop"
   ];
   remote = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA5QS/lsWUsrRtEhA2jVYYyevbeWOePNugR3QKHTZ+aG trev@build"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAxD3eccbwfEkahm6zLR+JIVnshwSBFO3dX3roFHndgp root@build"
   ];
 in {
   local_keys = local;
