fdb424a7bb77aff9736404891d0cf43546ddd9f3
trev <me@trev.xyz>
2025-05-29 05:02:06 -0400

    feat: re-key secrets

--- secrets/keys.nix
+++ secrets/keys.nix
@@ -8,6 +8,7 @@ let
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIInThVD92YRSlKIxCdhqLwsGkmvRUvRQFHwOuCQEOQlh root@media"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGQeBo8YspGiTDH3xhIg0vTWzwIOJtk3VeE6PQ97lyu4 root@gateway"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOFm+wetRXHfaW8yace4qW/zYG2+cqDWgTPgCOm8WHeX root@monitor"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIipGzt2u19Aon2qZaw8aVG1+ZRevX5t2LrpQBwt/WCG root@ai"
   ];
 in {
   local = local_keys;
