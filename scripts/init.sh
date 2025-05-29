253f16dc79b86105af15a8d48a824d72176459aa
trev <me@trev.xyz>
2025-05-29 04:59:52 -0400

    fix: generate host keys

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,6 +5,13 @@ if [[ -z "$1" ]]; then
   exit 1
 fi
 
+if ! [ -f /etc/ssh/ssh_host_rsa_key.pub ]; then
+  ssh-keygen -A
+  echo "Generated host keys, add this pubkey to secrets and re-run:"
+  cat /etc/ssh/ssh_host_rsa_key.pub
+  exit 1
+fi
+
 nix-shell -I nixpkgs=channel:nixos-unstable -p git --run "git clone https://github.com/spotdemo4/nix.git /etc/nixos"
 nixos-rebuild switch --flake "/etc/nixos#${1}" --accept-flake-config
 
