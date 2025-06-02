30c205cf72583469b86b7a9543be34ddc64be975
trev <me@trev.xyz>
2025-06-02 05:23:43 -0400

    rekey

--- secrets/keys.nix
+++ secrets/keys.nix
@@ -7,7 +7,7 @@ let
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAxD3eccbwfEkahm6zLR+JIVnshwSBFO3dX3roFHndgp root@build"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIInThVD92YRSlKIxCdhqLwsGkmvRUvRQFHwOuCQEOQlh root@media"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGQeBo8YspGiTDH3xhIg0vTWzwIOJtk3VeE6PQ97lyu4 root@gateway"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOFm+wetRXHfaW8yace4qW/zYG2+cqDWgTPgCOm8WHeX root@monitor"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBF162KlIQ0KM4MPHmSi9UEsDrVsdgiTyAdWSAOE87WE root@monitor"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIipGzt2u19Aon2qZaw8aVG1+ZRevX5t2LrpQBwt/WCG root@ai"
   ];
 in {
