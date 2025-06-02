7cce94ee8390088fa42b3eb387d0bdecee8825cd
trev <me@trev.xyz>
2025-06-01 23:40:36 -0400

    feat: zed alejandra

--- modules/home-manager/zed.nix
+++ modules/home-manager/zed.nix
@@ -1,7 +1,4 @@
-{
-  pkgs,
-  ...
-}: {
+{pkgs, ...}: {
   programs.zed-editor = {
     enable = true;
     extensions = [
@@ -12,12 +9,31 @@
     extraPackages = with pkgs; [
       nixd
       nil
+      alejandra
     ];
+    userSettings = {
+      buffer_font_family = "Fira Code";
+      languages = {
+        "Nix" = {
+          formatter = {
+            external = {
+              command = "alejandra";
+              arguments = [
+                "--quiet"
+                "--"
+              ];
+            };
+          };
+        };
+      };
+    };
   };
 
+  # Zed Theme
   catppuccin.zed = {
     enable = true;
     accent = "sky";
     flavor = "mocha";
+    italics = false;
   };
 }
