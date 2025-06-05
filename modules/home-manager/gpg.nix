222b59d28bd5572f080ebda46f5d70762dccb3e3
trev <me@trev.xyz>
2025-06-05 00:15:21 -0400

    fix: use correct directory

--- modules/home-manager/gpg.nix
+++ modules/home-manager/gpg.nix
@@ -4,6 +4,6 @@
   ...
 }: {
   age.secrets."gpg".file = self + /secrets/gpg.age;
-  age.secrets."gpg".path = config.home.homeDirectory + "/.gpupg/private-keys-v1.d/02F9D60E16452DC74C0FBFC2ECA9E20D1D75C89C.key";
+  age.secrets."gpg".path = config.home.homeDirectory + "/.gnupg/private-keys-v1.d/02F9D60E16452DC74C0FBFC2ECA9E20D1D75C89C.key";
   age.secrets."gpg".mode = "600";
 }
