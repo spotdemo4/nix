d33ee6a0c19d9e253b6f5ef1e99af1e6bece30c9
trev <me@trev.xyz>
2025-06-27 18:13:29 -0400

    fix: switch out docker image

--- servers/build.nix
+++ servers/build.nix
@@ -53,7 +53,7 @@
         tokenFile = config.age.secrets."codeberg".path;
         url = "https://codeberg.org";
         labels = [
-          "ubuntu-latest:docker://node:24-bookworm"
+          "ubuntu-latest:docker://ghcr.io/catthehacker/ubuntu:act-latest"
         ];
       };
     };
