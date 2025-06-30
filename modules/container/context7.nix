5cb70a57165bc704dffeca82e0704a568bef1896
trev <me@trev.xyz>
2025-06-30 16:20:51 -0400

    test: pin docker digests

--- modules/container/context7.nix
+++ modules/container/context7.nix
@@ -2,7 +2,7 @@
   toLabel = (import ./utils/toLabel.nix).toLabel;
 in {
   virtualisation.quadlet.containers.context7.containerConfig = {
-    image = "docker.io/mcp/context7:latest";
+    image = "docker.io/mcp/context7@sha256:1deaf09150eb2b84f845b132ccd61437cc9308f9d9d7b16aafd4c44a07625ca4:";
     pull = "newer";
     autoUpdate = "registry";
     publishPorts = [
