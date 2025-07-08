efb56850e36a070cef3ebd56382854010688d312
trev <me@trev.xyz>
2025-07-08 03:11:00 -0400

    feat: renovate overlay

--- flake.nix
+++ flake.nix
@@ -4,11 +4,13 @@
   nixConfig = {
     extra-substituters = [
       "https://trix.cachix.org"
+      "https://trevnur.cachix.org"
       "https://hyprland.cachix.org"
       "https://nix-community.cachix.org"
     ];
     extra-trusted-public-keys = [
       "trix.cachix.org-1:uZzPf9A0ij1eIlDn9jg7fZyxUGfbZrcRujVMIG6apVA="
+      "trevnur.cachix.org-1:hBd15IdszwT52aOxdKs5vNTbq36emvEeGqpb25Bkq6o="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
@@ -99,6 +101,9 @@
             inherit system;
             pkgs = import nixpkgs {
               inherit system;
+              overlays = [
+                nur.legacyPackages."${system}".repos.trev.overlays.renovate
+              ];
             };
           }
       );
