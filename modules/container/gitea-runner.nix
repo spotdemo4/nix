ed4c496caeab7b8d9f560506ef82fef6bafa80df
trev <me@trev.xyz>
2025-07-21 20:27:53 -0400

    feat: gitea runner cache

--- modules/container/gitea-runner.nix
+++ modules/container/gitea-runner.nix
@@ -1,4 +1,5 @@
 {
+  pkgs,
   config,
   lib,
   ...
@@ -18,28 +19,56 @@
   };
 
   config = lib.mkIf config.gitea-runner.enable {
-    virtualisation.quadlet.containers = lib.mapAttrs' (name: value:
-      lib.nameValuePair "runner-${name}" {
-        containerConfig = {
-          image = "docker.io/gitea/act_runner:0.2.12@sha256:62a561c82dd67ec77ea600db7eac78ac5fed8e2244950fbf1829c54da12e8e54";
-          pull = "missing";
-          environments = {
-            GITEA_INSTANCE_URL = value.url;
+    virtualisation.quadlet = let
+      inherit (config.virtualisation.quadlet) networks volumes;
+    in {
+      containers = lib.mapAttrs' (name: value:
+        lib.nameValuePair "runner-${name}" {
+          containerConfig = let
+            configFile = (pkgs.formats.yaml {}).generate "config.yaml" {
+              runner = {
+                capacity = 2;
+              };
+              cache = {
+                enabled = true;
+                dir = "/cache";
+                host = "runner-${name}";
+                port = 8088;
+              };
+              container = {
+                network = "runner-${name}";
+              };
+            };
+          in {
+            image = "docker.io/gitea/act_runner:0.2.12@sha256:62a561c82dd67ec77ea600db7eac78ac5fed8e2244950fbf1829c54da12e8e54";
+            pull = "missing";
+            environments = {
+              CONFIG_FILE = "/config.yaml";
+              GITEA_INSTANCE_URL = value.url;
+            };
+            volumes = [
+              "/run/podman/podman.sock:/var/run/docker.sock"
+              "${configFile}:/config.yaml"
+              "${volumes."runner-${name}".ref}:/cache"
+            ];
+            environmentFiles = [
+              value.tokenFile
+            ];
+            networks = [
+              networks."runner-${name}".ref
+            ];
           };
-          volumes = [
-            "/run/podman/podman.sock:/var/run/docker.sock"
-          ];
-          environmentFiles = [
-            value.tokenFile
-          ];
-        };
 
-        unitConfig = {
-          After = "podman.socket";
-          BindsTo = "podman.socket";
-          ReloadPropagatedFrom = "podman.socket";
-        };
-      })
-    config.gitea-runner.instances;
+          unitConfig = {
+            After = "podman.socket";
+            BindsTo = "podman.socket";
+            ReloadPropagatedFrom = "podman.socket";
+          };
+        })
+      config.gitea-runner.instances;
+
+      networks = lib.mapAttrs' (name: value: lib.nameValuePair "runner-${name}" {});
+      volumes = lib.mapAttrs' (name: value: lib.nameValuePair "runner-${name}" {});
+    };
   };
 }
