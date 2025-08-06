ace807a18c59dbb97e974a3d1392ed5887bc5239
trev <me@trev.xyz>
2025-08-06 02:17:26 -0400

    feat: discord-openrouter

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -10,12 +10,14 @@ in {
   "codeberg.age".publicKeys = keys.all;
   "continue.age".publicKeys = keys.local;
   "curseforge.age".publicKeys = keys.all;
+  "discord-openrouter.age".publicKeys = keys.all;
   "gitea-quanta.age".publicKeys = keys.all;
   "gitea.age".publicKeys = keys.all;
   "github-runner.age".publicKeys = keys.all;
   "gpg.age".publicKeys = keys.local;
   "grafana.age".publicKeys = keys.all;
   "mods.age".publicKeys = keys.local;
+  "openrouter.age".publicKeys = keys.all;
   "openwebui.age".publicKeys = keys.all;
   "tailscale.age".publicKeys = keys.all;
   "traefik.age".publicKeys = keys.all;
