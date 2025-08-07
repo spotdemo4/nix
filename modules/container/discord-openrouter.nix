e42effe2499967da6e7794cc780df6b242448188
trev <me@trev.xyz>
2025-08-06 21:32:12 -0400

    bump: discord-openrouter

--- modules/container/discord-openrouter.nix
+++ modules/container/discord-openrouter.nix
@@ -19,7 +19,7 @@ in {
 
   virtualisation.quadlet = {
     containers.discord-openrouter.containerConfig = {
-      image = "ghcr.io/spotdemo4/discord-openrouter:0.0.7@sha256:846494605283fae01edb19ddecaea63654573b173f6b23005d55d8221308a80f";
+      image = "ghcr.io/spotdemo4/discord-openrouter:0.0.8@sha256:d2ba2920a023ee2eb410625d2a8a2ca1c76615479f6bc52a53c296f7eb6a71c0";
       pull = "missing";
       environments = {
         DEFAULT_PROMPT = "don't worry about formalities. don't use emojis. don't be cringe. be as terse as possible while still conveying substantially all information relevant to any question. critique freely and avoid sycophancy. don't be afraid to use profanity, especially to convey frustration or intensity. don't be afraid to take a side in any discussion, especially if one side is clearly correct. cite sources for your claims if possible. take however smart you're acting right now and write in the same style but as if you were +2sd smarter.";
