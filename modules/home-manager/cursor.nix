204d537083ab8449a93de20be9f6aa2e65a2c627
trev <me@trev.xyz>
2025-08-14 19:09:26 -0400

    fix: use hyprcursor

--- modules/home-manager/cursor.nix
+++ modules/home-manager/cursor.nix
@@ -5,4 +5,10 @@
     flavor = "mocha";
   };
   home.pointerCursor.size = 22;
+
+  # for hyprland
+  home.sessionVariables = {
+    HYPRCURSOR_SIZE = 22;
+    HYPRCURSOR_THEME = "catppuccin-dark-mocha-cursors";
+  };
 }
