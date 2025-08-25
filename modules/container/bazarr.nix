bac7e836a7301a123a44914fb5baee65dd533b40
trev <me@trev.xyz>
2025-08-25 13:35:53 -0400

    feat: bazarr

---
+++ modules/container/bazarr.nix
@@ -0,0 +1,43 @@
+{
+  config,
+  self,
+  ...
+}: let
+  inherit (config.virtualisation.quadlet) volumes;
+  toLabel = import (self + /modules/util/label);
+in {
+  virtualisation.quadlet = {
+    containers.bazarr.containerConfig = {
+      image = "lscr.io/linuxserver/bazarr:1.5.2@sha256:d37a88fb1751c7b9a9fc4d134b8e96c1e32821ee38630770e6a9112cf47fbe55";
+      pull = "missing";
+      environments = {
+        PUID = "1000";
+        PGID = "1000";
+        TZ = "America/Detroit";
+      };
+      volumes = [
+        "${volumes.bazarr.ref}:/config"
+        "/mnt/pool/movies:/movies"
+        "/mnt/pool/shows:/shows"
+      ];
+      publishPorts = [
+        "6767"
+      ];
+      labels = toLabel {
+        attrs = {
+          traefik = {
+            enable = true;
+            http.routers.bazarr = {
+              rule = "HostRegexp(`bazarr.trev.(zip|kiwi)`)";
+              middlewares = "auth-github@docker";
+            };
+          };
+        };
+      };
+    };
+
+    volumes = {
+      bazarr = {};
+    };
+  };
+}
