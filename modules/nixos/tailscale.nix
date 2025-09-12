d0e2e62f138b66c3baff59e73895991e949c917f
trev <me@trev.xyz>
2025-09-12 11:16:12 -0400

    fix: don't check tailscale

--- modules/nixos/tailscale.nix
+++ modules/nixos/tailscale.nix
@@ -1,6 +1,7 @@
 {
   config,
   self,
+  pkgs,
   ...
 }: {
   age.secrets."tailscale".file = self + /secrets/tailscale.age;
@@ -8,5 +9,7 @@
   services.tailscale = {
     enable = true;
     authKeyFile = config.age.secrets."tailscale".path;
+    # https://github.com/NixOS/nixpkgs/issues/438765#issuecomment-3239816312
+    package = pkgs.tailscale.overrideAttrs {doCheck = false;};
   };
 }
