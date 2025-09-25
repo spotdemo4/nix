e71706a94ccf180a7961794443838751ab6b1305
trev <me@trev.xyz>
2025-09-25 05:08:17 -0400

    fix: set user & group

--- modules/container/traefik.nix
+++ modules/container/traefik.nix
@@ -247,6 +247,8 @@ in {
       traefik-certs-dumper.containerConfig = {
         image = "ghcr.io/kereis/traefik-certs-dumper:1.8.10@sha256:c5bbc45fb631c70ff15f3dd2fde8486902d28e933c40cbbdd7988a4c9d4b84eb";
         pull = "missing";
+        user = "1000";
+        group = "1000";
         addCapabilities = [
           "CAP_DAC_OVERRIDE"
         ];
