e0660352e845d731858991442a024914413da1a7
trev <me@trev.xyz>
2025-10-08 15:19:36 -0400

    fix: remove ovmf

--- modules/nixos/virt-manager.nix
+++ modules/nixos/virt-manager.nix
@@ -1,12 +1,8 @@
-{pkgs, ...}: {
+{...}: {
   virtualisation.libvirtd = {
     enable = true;
     qemu = {
       swtpm.enable = true;
-      ovmf = {
-        enable = true;
-        packages = [pkgs.OVMFFull.fd];
-      };
     };
   };
   programs.virt-manager.enable = true;
