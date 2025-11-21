a95a09b2c098e00811ea14042e128b90b6c9c475
trev <me@trev.xyz>
2025-11-21 08:29:14 -0500

    fix: immich, rekey

--- modules/container/immich/default.nix
+++ modules/container/immich/default.nix
@@ -30,44 +30,57 @@ in
   };
 
   virtualisation.quadlet = {
-    containers.immich.containerConfig = {
-      image = "ghcr.io/imagegenius/immich:2.3.1@sha256:447ef17217ca08588d787249b65a0bb988e942449c8c047515c8da3cab67824d";
-      pull = "missing";
-      devices = [
-        "/dev/dri/card0:/dev/dri/card0"
-        "/dev/dri/renderD128:/dev/dri/renderD128"
-      ];
-      environments = {
-        PUID = "1000";
-        PGID = "1000";
-        TZ = "America/Detroit";
+    containers.immich = {
+      containerConfig = {
+        image = "ghcr.io/imagegenius/immich:2.3.1@sha256:447ef17217ca08588d787249b65a0bb988e942449c8c047515c8da3cab67824d";
+        pull = "missing";
+        devices = [
+          "/dev/dri/card0:/dev/dri/card0"
+          "/dev/dri/renderD128:/dev/dri/renderD128"
+        ];
+        environments = {
+          PUID = "1000";
+          PGID = "1000";
+          TZ = "America/Detroit";
 
-        DB_HOSTNAME = postgresql."immich".ref;
-        DB_USERNAME = postgresql."immich".username;
-        DB_PASSWORD = postgresql."immich".password;
-        DB_DATABASE_NAME = postgresql."immich".database;
+          DB_HOSTNAME = postgresql."immich".ref;
+          DB_USERNAME = postgresql."immich".username;
+          DB_PASSWORD = postgresql."immich".password;
+          DB_DATABASE_NAME = postgresql."immich".database;
 
-        REDIS_HOSTNAME = valkey."immich".ref;
-      };
-      volumes = [
-        "${volumes."immich".ref}:/config"
-        "/mnt/pool/photos:/photos"
-      ];
-      publishPorts = [
-        "8080"
-      ];
-      networks = [
-        networks."immich".ref
-      ];
-      labels = toLabel {
-        attrs = {
-          traefik = {
-            enable = true;
-            http.routers.immich = {
-              rule = "HostRegexp(`photos.trev.(xyz|zip)`)";
+          REDIS_HOSTNAME = valkey."immich".ref;
+        };
+        volumes = [
+          "${volumes."immich".ref}:/config"
+          "/mnt/photos:/photos"
+        ];
+        publishPorts = [
+          "8080"
+        ];
+        networks = [
+          networks."immich".ref
+        ];
+        labels = toLabel {
+          attrs = {
+            traefik = {
+              enable = true;
+              http.routers.immich = {
+                rule = "HostRegexp(`photos.trev.(xyz|zip)`)";
+              };
             };
           };
         };
+      };
+
+      unitConfig = {
+        After = [
+          postgresql."immich".ref
+          valkey."immich".ref
+        ];
+        BindsTo = [
+          postgresql."immich".ref
+          valkey."immich".ref
+        ];
       };
     };
 
