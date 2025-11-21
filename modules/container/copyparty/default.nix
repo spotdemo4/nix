9531c6f4c9dbdd251c85284d3ea3794a0c5c65ae
trev <me@trev.xyz>
2025-11-21 03:23:03 -0500

    fix: config

--- modules/container/copyparty/default.nix
+++ modules/container/copyparty/default.nix
@@ -8,8 +8,9 @@ let
   inherit (config) secrets;
   toLabel = import (self + /modules/util/label);
 
+  accounts = "/accounts.conf";
   cfg = pkgs.replaceVars ./copyparty.conf {
-    accounts = secrets."copyparty".path;
+    accounts = accounts;
   };
 in
 {
@@ -20,6 +21,9 @@ in
   virtualisation.quadlet.containers.copyparty.containerConfig = {
     image = "ghcr.io/9001/copyparty-ac:1.19.20@sha256:81bae1a42e0ad879231f84eeb88f6a8b7dc943e84c1fb838dd44ccfac0fffc5a";
     pull = "missing";
+    secrets = [
+      "${secrets."copyparty".mount},target=${accounts}"
+    ];
     volumes = [
       "/mnt/files:/w"
       "${cfg}:/cfg/copyparty.conf"
