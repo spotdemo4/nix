0d6e37883c1d6d8228b0afbd6d6cdef8a312640f
spotdemo4 <me@trev.xyz>
2025-12-18 13:49:35 -0500

    fix(metrics): monitor correctly

---
+++ modules/container/victoria-metrics/default.nix
@@ -0,0 +1,49 @@
+{
+  config,
+  self,
+  ...
+}:
+let
+  inherit (config.virtualisation.quadlet) networks volumes;
+  toLabel = import (self + /modules/util/label);
+in
+{
+  virtualisation.quadlet = {
+    containers.victoria-metrics.containerConfig = {
+      image = "docker.io/victoriametrics/victoria-metrics:v1.132.0@sha256:6bf82afbd51dcdadf5f908d80e38c78be01232aa94f945ce93fae99daa84d0d9";
+      pull = "missing";
+      volumes = [
+        "${volumes."victoria-metrics".ref}:/victoria-metrics-data"
+        "${./prometheus.yaml}:/prometheus.yaml"
+      ];
+      publishPorts = [
+        "8428:8428"
+      ];
+      networks = [
+        networks."victoria-metrics".ref
+      ];
+      exec = [
+        "--selfScrapeInterval=5s"
+        "-storageDataPath=victoria-metrics-data"
+        "-promscrape.config=prometheus.yaml"
+      ];
+      labels = toLabel {
+        attrs.traefik = {
+          enable = true;
+          http.routers.victoria-metrics = {
+            rule = "HostRegexp(`victoria-metrics.trev.(zip|kiwi)`)";
+            middlewares = "secure-trev@file";
+          };
+        };
+      };
+    };
+
+    networks = {
+      victoria-metrics = { };
+    };
+
+    volumes = {
+      victoria-metrics = { };
+    };
+  };
+}
