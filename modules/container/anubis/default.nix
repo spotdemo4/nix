210382d264f563b92141ceca45b98792eeee11fe
spotdemo4 <me@trev.xyz>
2025-12-19 12:37:27 -0500

    fix: funcs

--- modules/container/anubis/default.nix
+++ modules/container/anubis/default.nix
@@ -5,7 +5,7 @@
 let
   toLabel = import (self + /modules/util/label);
   mkAnubis = domain: suffix: port: {
-    virtualisation.quadlet.containers."anubis-${suffix}".containerConfig = {
+    "anubis-${suffix}".containerConfig = {
       image = "ghcr.io/techarohq/anubis:latest@sha256:170d30f7de14f6b19e5d7647a1ea8ff58740828a08a6a364e08210df2955639b";
       pull = "missing";
       environments = {
@@ -36,15 +36,6 @@ let
   };
 in
 {
-  imports = [
-    mkAnubis
-    "trev.zip"
-    "zip"
-    "8080"
-
-    mkAnubis
-    "trev.xyz"
-    "xyz"
-    "8081"
-  ];
+  virtualisation.quadlet.containers =
+    mkAnubis "trev.zip" "zip" "8080" // mkAnubis "trev.xyz" "xyz" "8081";
 }
