1638c06ae43aa6733e5c9e162e6930673cef3944
spotdemo4 <me@trev.xyz>
2025-12-19 12:44:13 -0500

    fix: container syntax

--- modules/container/anubis/default.nix
+++ modules/container/anubis/default.nix
@@ -5,7 +5,7 @@
 let
   toLabel = import (self + /modules/util/label);
   mkAnubis = domain: suffix: port: {
-    "anubis-${suffix}".containerConfig = {
+    containerConfig = {
       image = "ghcr.io/techarohq/anubis:latest@sha256:170d30f7de14f6b19e5d7647a1ea8ff58740828a08a6a364e08210df2955639b";
       pull = "missing";
       environments = {
@@ -36,6 +36,8 @@ let
   };
 in
 {
-  virtualisation.quadlet.containers =
-    mkAnubis "trev.zip" "zip" "8080" // mkAnubis "trev.xyz" "xyz" "8081";
+  virtualisation.quadlet.containers = {
+    "anubis-zip" = mkAnubis "trev.zip" "zip" "8080";
+    "anubis-xyz" = mkAnubis "trev.xyz" "xyz" "8081";
+  };
 }
