2e1b6993d17f69d8120ed6620fa39f4dba7b7451
spotdemo4 <me@trev.xyz>
2026-02-06 03:15:16 -0500

    fix(crowdsec): ignore garage & niks3

--- modules/container/crowdsec/default.nix
+++ modules/container/crowdsec/default.nix
@@ -20,6 +20,8 @@ in
         "${./traefik.yaml}:/etc/crowdsec/acquis.d/traefik.yaml:ro"
         "${./attic.yaml}:/etc/crowdsec/parsers/s02-enrich/attic.yaml:ro"
         "${./versitygw.yaml}:/etc/crowdsec/parsers/s02-enrich/versitygw.yaml:ro"
+        "${./garage.yaml}:/etc/crowdsec/parsers/s02-enrich/garage.yaml:ro"
+        "${./niks3.yaml}:/etc/crowdsec/parsers/s02-enrich/niks3.yaml:ro"
       ];
       publishPorts = [
         "6061:8080" # api
