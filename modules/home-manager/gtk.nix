c4f98870fa705d1749fbcb41bde787ff52c511d4
spotdemo4 <me@trev.xyz>
2026-04-05 01:54:44 -0400

    fix: catppuccin-gtk

--- modules/home-manager/gtk.nix
+++ modules/home-manager/gtk.nix
@@ -1,17 +1,17 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   gtk = {
     enable = true;
     theme = {
-      package = pkgs.colloid-gtk-theme.override {
-        tweaks = [ "catppuccin" ];
+      package = pkgs.catppuccin-gtk.override {
+        themeVariants = [ "sky" ];
         colorVariants = [ "dark" ];
-        themeVariants = [ "teal" ];
       };
-      name = "Colloid-Teal-Dark-Catppuccin";
+      name = "Catppuccin-Sky-Dark";
     };
+
     # https://raw.githubusercontent.com/nix-community/home-manager/f2d3e04e278422c7379e067e323734f3e8c585a7/modules/misc/news/2025/11/2025-11-26_11-55-28.nix
-    gtk4.theme = null;
+    gtk4.theme = config.gtk.theme;
   };
 
   catppuccin.gtk.icon = {
