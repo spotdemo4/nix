8eb9a775cf7ba3ebd5cb8802ea9adb2e3b7e898d
trev <me@trev.xyz>
2026-05-05 04:43:12 -0400

    feat: more zed stuff

--- modules/home-manager/zed/default.nix
+++ modules/home-manager/zed/default.nix
@@ -1,16 +1,29 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.zed-editor = {
     enable = true;
     extensions = [
+      "gleam"
       "html"
       "nix"
+      "proto"
+      "ruff"
       "sql"
       "svelte"
       "toml"
+      "zig"
+    ];
+    extraPackages = with pkgs; [
+      nixd
+      nil
+      claude-code
     ];
     mutableUserSettings = false;
-    userSettings = builtins.fromJSON (builtins.readFile ./settings.json);
+    userSettings = builtins.fromJSON (
+      pkgs.replaceVars ./settings.json {
+        claude = "${pkgs.claude-code}/bin/claude";
+      }
+    );
   };
 
   # Zed Theme
@@ -19,5 +32,10 @@
     accent = "sky";
     flavor = "mocha";
     italics = false;
+
+    icons = {
+      enable = true;
+      flavor = "mocha";
+    };
   };
 }
