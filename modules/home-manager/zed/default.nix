b411eb391815200a89bc72e99af2bab251c24e67
spotdemo4 <me@trev.xyz>
2026-05-25 02:43:06 -0400

    feat: zed keymaps

--- modules/home-manager/zed/default.nix
+++ modules/home-manager/zed/default.nix
@@ -20,7 +20,9 @@
       "zig"
     ];
     mutableUserSettings = false;
+    mutableUserKeymaps = false;
     userSettings = builtins.fromJSON (builtins.readFile ./settings.json);
+    userKeymaps = builtins.fromJSON (builtins.readFile ./keymap.json);
   };
 
   home.packages = with pkgs; [
