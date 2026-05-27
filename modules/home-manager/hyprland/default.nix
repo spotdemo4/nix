92d49d2f956c93e9f360eb5d722b23332d17e923
trev <me@trev.xyz>
2026-05-27 11:21:30 -0400

    test: use xdg-desktop-portal-luminous

--- modules/home-manager/hyprland/default.nix
+++ modules/home-manager/hyprland/default.nix
@@ -2,9 +2,17 @@
 {
   wayland.windowManager.hyprland = {
     enable = true;
-    portalPackage = pkgs.trev.xdg-desktop-portal-hyprland;
     configType = "lua";
     extraConfig = builtins.readFile ./settings.lua;
+  };
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [ xdg-desktop-portal-luminous ];
+    config.hyprland.default = [
+      "hyprland"
+      "luminous"
+    ];
   };
 
   home.packages = with pkgs; [
