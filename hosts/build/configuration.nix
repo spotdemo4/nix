06eeed6b104db3f16393a80c0c06e7441dc3780d
spotdemo4 <me@trev.xyz>
2026-07-06 09:16:57 -0400

    fix(build): set chromium runner work directory

--- hosts/build/configuration.nix
+++ hosts/build/configuration.nix
@@ -82,6 +82,7 @@
     chromium-android-desktop = {
       enable = true;
       url = "https://github.com/spotdemo4/chromium-android-desktop";
+      workDir = "/home/github-runner/chromium";
       tokenFile = config.age.secrets."github-runner".path;
       name = "builder";
       replace = true;
