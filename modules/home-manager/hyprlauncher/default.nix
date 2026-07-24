df10a8fcfffb6007e6cee295e2603bead99a272c
trev <me@trev.xyz>
2026-07-24 15:15:22 -0400

    feat(hyprlauncher): add default config files

--- modules/home-manager/hyprlauncher/default.nix
+++ modules/home-manager/hyprlauncher/default.nix
@@ -10,7 +10,14 @@
   config = lib.mkIf config.trev.programs.hyprlauncher.enable {
     home.packages = [ pkgs.hyprlauncher ];
 
+    xdg.configFile."hypr/hyprlauncher.conf".text = ''
+      ui {
+        window_size = 500 325
+      }
+    '';
+
     xdg.configFile."hypr/hyprtoolkit.conf".text = ''
+      background = rgba(0, 0, 0, 0.4)
       accent = rgba(33ccffee)
       accent_secondary = rgba(00ff99ee)
     '';
