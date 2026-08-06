cdade477b93b53adfe6a23040fd613aaf6d9f083
trev <me@trev.xyz>
2026-08-06 13:38:40 -0400

    feat(containers): pin runner images by architecture

--- hosts/build/containers.nix
+++ hosts/build/containers.nix
@@ -5,9 +5,10 @@
 }:
 let
   labels = [
-    "ubuntu-latest:docker://gitea/runner-images:ubuntu-latest@sha256:e77e2b1ebba51adb1c59d8eb185bc54e397b7e22442756aa7ea0e7b841fd2906"
-    "ubuntu-24.04:docker://gitea/runner-images:ubuntu-24.04@sha256:e77e2b1ebba51adb1c59d8eb185bc54e397b7e22442756aa7ea0e7b841fd2906"
-    "nixos-latest:docker://nixos/nix:2.35.1@sha256:377d4887aca98f0dfa12971c1ea6d6a625a435d8b610d4c95a436843da6fbfd1"
+    "ubuntu-latest:docker://gitea/runner-images:ubuntu-latest@sha256:e77e2b1ebba51adb1c59d8eb185bc54e397b7e22442756aa7ea0e7b841fd2906" # arch=amd64
+    "ubuntu-24.04:docker://gitea/runner-images:ubuntu-24.04@sha256:e77e2b1ebba51adb1c59d8eb185bc54e397b7e22442756aa7ea0e7b841fd2906" # arch=amd64
+    "ubuntu-24.04-arm:docker://gitea/runner-images:ubuntu-24.04@sha256:e34fb27ad655bf1fb20d1ae6bbb71650ca29f286b3cf67e3e86dd91a90fd2976" # arch=arm64
+    "nixos-latest:docker://nixos/nix:2.35.1@sha256:377d4887aca98f0dfa12971c1ea6d6a625a435d8b610d4c95a436843da6fbfd1" # arch=amd64
   ];
   runner = {
     enable = true;
