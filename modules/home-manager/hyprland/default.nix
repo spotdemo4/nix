1588b088072c794cdb965128ae44ce6bfbbb6513
trev <me@trev.xyz>
2026-08-30 04:22:44 -0400

    chore(hyprland): use packaged luminous portal

--- modules/home-manager/hyprland/default.nix
+++ modules/home-manager/hyprland/default.nix
@@ -7,25 +7,6 @@
 }:
 let
   trevbarPackage = inputs.trevbar.packages.${pkgs.stdenv.hostPlatform.system}.default;
-
-  # Temporary override for https://github.com/waycrate/xdg-desktop-portal-luminous/pull/217
-  luminousPackage = pkgs.trev.xdg-desktop-portal-luminous.overrideAttrs (
-    finalAttrs: _previousAttrs: {
-      version = "0.1.21-pr217-f13071e";
-
-      src = pkgs.fetchFromGitHub {
-        owner = "waycrate";
-        repo = "xdg-desktop-portal-luminous";
-        rev = "f13071ebf810c6cf181b18ff8c7c3c76702d581a";
-        hash = "sha256-6GD/6412KIlNgGc8EmNJiNQrFsj/Jec94Y+gHYnyBXw=";
-      };
-
-      cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
-        inherit (finalAttrs) pname version src;
-        hash = "sha256-N6TteUB3AC+LNxglvgQQ4RxRSaZk6E0f42BbYd3d+Rk=";
-      };
-    }
-  );
 in
 {
   options.trev.programs.hyprland.enable = lib.mkEnableOption "Trev's Hyprland configuration";
@@ -59,7 +40,7 @@ in
       enable = true;
       extraPortals = with pkgs; [
         xdg-desktop-portal-gtk
-        luminousPackage
+        xdg-desktop-portal-luminous
       ];
       config.hyprland = {
         default = [
